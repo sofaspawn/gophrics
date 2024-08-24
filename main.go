499e8bb2f85e023e1f2c2ce7b6ebc1202b328e94
sofaspawn <h.m1nus.work@gmail.com>
2024-08-24 08:53:45 +0530

    conservation of energy

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"image/color"
 	"log"
+	"math"
 	"math/rand"
 
 	"github.com/hajimehoshi/ebiten/v2"
@@ -15,45 +16,76 @@ type Game struct {
 	particles []*Particle
 	width     int
 	height    int
+	g         float32
 }
 
 type Particle struct {
 	x_pos         float32
 	y_pos         float32
 	radius        float32
+	mass          float32
+	velocity      float64
 	color         color.Color
 	anti_aliasing bool
 	direction     [2]float32
+	potential     float32
+	total         float32
+	//kinetic       float32
 }
 
 func (g *Game) gameinitconfig() {
+	g.g = 10
 	g.width, g.height = ebiten.WindowSize()
-    random_x := float32(g.width) * rand.Float32()
-    random_y := float32(g.height) * rand.Float32()
-    g.addparticle(float32(g.width)/2.0, float32(g.width)/2.0, 50, [4]uint8{0, 255, 0, 0}, true, [2]float32{10*rand.Float32(),10*rand.Float32()})
-    g.addparticle(random_x, random_y, 50, [4]uint8{0, 0, 255, 0}, true, [2]float32{10*rand.Float32(),10*rand.Float32()})
-    g.addparticle(random_x, random_y, 50, [4]uint8{255, 0, 0, 0}, true, [2]float32{10*rand.Float32(),10*rand.Float32()})
+	random_x := float32(g.width) * rand.Float32()
+	random_y := float32(g.height) * rand.Float32()
+	var velocity float64 = 5
+
+	g.addparticle(random_x, random_y, 50, [4]uint8{0, 255, 0, 0}, true, [2]float32{10 * rand.Float32(), 10 * rand.Float32()}, velocity)
+	g.addparticle(random_x, random_y, 50, [4]uint8{0, 0, 255, 0}, true, [2]float32{10 * rand.Float32(), 10 * rand.Float32()}, velocity)
+	g.addparticle(random_x, random_y, 50, [4]uint8{255, 0, 0, 0}, true, [2]float32{10 * rand.Float32(), 10 * rand.Float32()}, velocity)
+
 	g.isinit = true
 }
 
-func (g *Game) addparticle(x_pos, y_pos, radius float32, c [4]uint8, anti_aliasing bool, direction [2]float32){
-    particle := Particle{}
-    particle.x_pos = x_pos
-    particle.y_pos = y_pos
-    particle.radius = radius
-    particle.color = color.RGBA{c[0], c[1], c[2], c[3]}
-    particle.anti_aliasing = anti_aliasing
-    particle.direction = direction
-    g.particles = append(g.particles, &particle)
+func (g *Game) addparticle(x_pos, y_pos, radius float32, c [4]uint8, anti_aliasing bool, direction [2]float32, velocity float64) {
+	particle := Particle{}
+
+	particle.x_pos = x_pos
+	particle.y_pos = y_pos
+	particle.radius = radius
+	particle.color = color.RGBA{c[0], c[1], c[2], c[3]}
+	particle.anti_aliasing = anti_aliasing
+	particle.direction = direction
+	particle.velocity = velocity
+
+	g.particles = append(g.particles, &particle)
+}
+
+// FIXME: fix the gravity function
+/*
+func (g *Game) gravity(){
+    g.g = 10
+    for _, particle := range g.particles{
+        particle.direction[0] *=
+    }
+}
+*/
+
+func (g *Game) consvOfEnergy() {
+	for _, particle := range g.particles {
+		particle.potential = (particle.mass) * (g.g) * (float32(g.width) - particle.y_pos)
+		particle.total = particle.mass * (g.g) * float32(g.height)
+		particle.velocity = math.Sqrt(float64(2/particle.mass) * float64(particle.total-particle.potential))
+	}
 }
 
 func bounce(particle *Particle) {
 	width, height := ebiten.WindowSize()
 	if particle.x_pos+particle.radius >= float32(width) || particle.x_pos-particle.radius <= 0 {
-		particle.direction[0] = -1 * particle.direction[0]
+		particle.direction[0] *= -1
 	}
 	if particle.y_pos+particle.radius >= float32(height) || particle.y_pos-particle.radius <= 0 {
-		particle.direction[1] = -1 * particle.direction[1]
+		particle.direction[1] *= -1
 	}
 }
 
@@ -63,15 +95,16 @@ func (g *Game) Update() error {
 		return ebiten.Termination
 	}
 
-    for _, particle := range g.particles{
-        bounce(particle)
-        if g.timer%1 == 0 {
-            i, j := particle.direction[0], particle.direction[1]
-            inc_p0 := 10
-            particle.x_pos += i * float32(inc_p0)
-            particle.y_pos += j * float32(inc_p0)
-        }
-    }
+	for _, particle := range g.particles {
+		bounce(particle)
+		g.consvOfEnergy()
+		if g.timer%1 == 0 {
+			i, j := particle.direction[0], particle.direction[1]
+
+			particle.x_pos += i * float32(particle.velocity)
+			particle.y_pos += j * float32(particle.velocity)
+		}
+	}
 
 	return nil
 }
@@ -82,9 +115,9 @@ func (g *Game) Draw(screen *ebiten.Image) {
 		g.gameinitconfig()
 	}
 	if len(g.particles) > 0 && g.particles[0] != nil {
-        for _, particle := range g.particles{
-            vector.DrawFilledCircle(screen, particle.x_pos, particle.y_pos, particle.radius, particle.color, particle.anti_aliasing)
-        }
+		for _, particle := range g.particles {
+			vector.DrawFilledCircle(screen, particle.x_pos, particle.y_pos, particle.radius, particle.color, particle.anti_aliasing)
+		}
 	}
 	//fmt.Println(g.particles[0])
 }
